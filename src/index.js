87d93530231e3526ba0696c42b7d872bdd8b6cf9
kouts <koutsaftakis@gmail.com>
2020-11-14 17:43:57 +0200

    Added css styling for app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import _ from 'lodash'
+import './style.css'
 
+const template = (title) => `
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1>${title}</h1>
+      <p class="lead">
+        This is the landing page of the application...
+      </p>
+    </div>
+  </section>
+`
 function component () {
   const element = document.createElement('div')
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
+  const title = _.join(['Welcome', 'to the app'], ' ')
+  element.innerHTML = template(title)
   return element
 }
 
-document.body.appendChild(component())
+document.querySelector('#app').appendChild(component())
