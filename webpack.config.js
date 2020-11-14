079c8c4fe09ec2387f700f9208ce85a3e5f15f05
kouts <koutsaftakis@gmail.com>
2020-11-14 17:58:58 +0200

    Added Babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       }
